93a11b06bcc22e930b84baa613c06667be9d2921
h1J4cker <33754132+Survive2@users.noreply.github.com>
2022-06-24 18:22:55 +0800

    Add files via upload

---
+++ quick_patchelf.py
@@ -0,0 +1,73 @@
+from re import *
+import os
+import argparse
+import sys
+from sgtpyutils.logger import logger
+from sgtpyutils.xls_txt.list import list2sheet
+
+
+libc_all_in_one=f'/home/kali/Desktop/glibc-all-in-one/libs'
+start_path=f'/home/kali/Desktop'
+
+def process_cmd(cmd):
+    logger.info(f'exec command: {cmd}')
+    os.system(cmd)
+
+def process_patchelf(libc_file,ld_file,elf_file):
+    libc_file_path=f'{libc_all_in_one}/{libc_file}'
+    elf_file_path=f'{start_path}/{elf_file}'
+    ld_file_path=f'{libc_all_in_one}/{ld_file}'
+    cmd=f'patchelf --set-interpreter {ld_file_path} {elf_file_path}'
+    process_cmd(cmd)
+    cmd=f'patchelf --set-rpath {libc_file_path} {elf_file_path}'
+    process_cmd(cmd)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description="Patchelf Quickly!"
+            )
+    parser.add_argument(
+            '-libc',
+            default='none',
+            help="the libc file you want"
+            )
+    parser.add_argument(
+            '-ld',
+            default='None',
+            help="the ld file you want"
+            )
+
+    parser.add_argument(
+            '-elf',
+            default='None',
+            help="the target elf file"
+            )
+
+    parser.add_argument(
+            '-l',
+            default=False,
+            nargs=argparse.OPTIONAL,
+            dest='show_all_libc',
+            help="show the avaliable libc"
+            )
+
+
+    args = parser.parse_args()
+
+    libc_list=os.listdir(libc_all_in_one)
+    libc_list=sorted(libc_list)
+    
+    if(args.show_all_libc or args.show_all_libc is None):
+        libc_list= list2sheet(libc_list, max_show_count=20)
+        description = ['\navaliable libc list:']
+        description.append('\n'.join(libc_list))
+        logger.debug('\n'.join(description))
+        sys.exit(0)
+
+    process_patchelf(
+            args.libc,
+            args.ld,
+            args.elf,
+            )
+
+    logger.info(f'sucessfully patch {args.elf} libc to {args.libc}')
